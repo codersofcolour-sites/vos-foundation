f5370601457f0f13786bc774560e2ee446e1414d
A.A.Isa <aabdulmajeed.isa@gmail.com>
2020-09-02 10:17:49 +0100

    improve home and base html

--- about/models.py
+++ about/models.py
@@ -9,11 +9,3 @@ from wagtail.admin.edit_handlers import StreamFieldPanel
 class AboutPage(Page):   
     subpage_types = ['trustees.TrusteesPage', 'history.HistoryPage', 'beneficiaries.BeneficiariesPage', 'supporters.SupportersPage']
     max_count = 1 
-    about_content = StreamField([
-        ('subheading', blocks.RichTextBlock(icon="title", features=['h2'])),
-        ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['p','link', 'bold', 'italic'])),
-    ], null=True)
-
-    content_panels = Page.content_panels + [
-        StreamFieldPanel('about_content')            
-    ]
